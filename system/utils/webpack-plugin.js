b852eec5f1b98b962d2b19b933fea8f8803916de
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-02-02 10:38:01 +0800

    added fonts
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- system/utils/webpack-plugin.js
+++ system/utils/webpack-plugin.js
@@ -30,6 +30,10 @@ module.exports = (env) => {
         to: 'images'
       },
       {
+        from: path.resolve(__dirname, '../../app/fonts'),
+        to: 'fonts'
+      },
+      {
         from: path.resolve(__dirname, '../../data'),
         to: 'data'
       }
