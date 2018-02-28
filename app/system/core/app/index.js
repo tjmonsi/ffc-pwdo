a8559bddb3b196457e281ddc2b3bf12bbbd1a62d
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-02-28 21:48:35 +0800

    edited some parts
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- app/system/core/app/index.js
+++ app/system/core/app/index.js
@@ -1,3 +1,3 @@
 
-export default {"title":"Form Function & Class 9 &middot; Web Design and Front-End Masterclass &middot; August 18, 2018","shortTitle":"FFC 9","description":"Advanced techniques in designing &amp; developing for the web, taught by the best in the world.","twitter":"","image":"https://ffc-site.firebaseapp.com/images/opimage.png"}
+export default {"title":"Form Function & Class 9 &middot; Web Design and Front-End Masterclass &middot; August 18, 2018","shortTitle":"FFC 9","description":"Advanced techniques in designing &amp; developing for the web, taught by the best in the world.","twitter":"@ffcph","image":"https://ffc-site.firebaseapp.com/images/opimage.png"}
   
