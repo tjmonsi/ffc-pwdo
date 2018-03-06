8f6e6d06196dd2e6c23ec32c5d395374c5689db3
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-03-06 17:51:33 +0800

    finished

--- app/shell/index.js
+++ app/shell/index.js
@@ -1,3 +1,20 @@
 import 'core/components/lazy-loader';
 import '@littleq/state-manager';
 import '@littleq/littleq-router';
+
+const script = document.createElement('script');
+script.src = 'https://use.typekit.net/fed2dzd.js';
+script.onload = () => {
+  try {
+    window.Typekit.load({
+      async: true
+    });
+    console.log('start');
+    const style = document.createElement('style');
+    style.textContent = 'h1, h2, h3, h4, h5, h6 { font-family: "acumin-pro", "Acumin Pro", "Helvetica Neue", Helvetica, Arial, sans-serif; font-weight: 800; }'
+    document.head.appendChild(style);
+  } catch (e) {
+    console.log(e);
+  }
+};
+document.body.appendChild(script);
